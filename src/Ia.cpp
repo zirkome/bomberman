052f9f456913346e20369a4f73e0e63deed13d4a
Guillaume Fillon <guillaume@posteo.de>
2014-05-05 11:55:17 +0200

    Removed EPITECH's header

--- src/Ia.cpp
+++ src/Ia.cpp
@@ -1,13 +1,3 @@
-/*
-** Ia.cpp for bomberman in /home/max/projet/actu/bomberman
-**
-** Made by Maxime
-** Login   <max@epitech.net>
-**
-** Started on  Mon May  5 11:28:32 2014 Maxime
-** Last update Mon May  5 11:36:52 2014 Maxime
-*/
-
 #include "Ia.hpp"
 
 Ia::Ia()
