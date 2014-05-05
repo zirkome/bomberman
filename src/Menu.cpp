071839ed2d176c0b81684e5e08ebdaf05d788691
maxime94 <maxime.l@live.fr>
2014-05-05 11:40:59 +0200

    add some class

---
+++ src/Menu.cpp
@@ -0,0 +1,19 @@
+/*
+** Menu.cpp for bomberman in /home/max/projet/actu/bomberman
+**
+** Made by Maxime
+** Login   <max@epitech.net>
+**
+** Started on  Mon May  5 11:28:32 2014 Maxime
+** Last update Mon May  5 11:31:50 2014 Maxime
+*/
+
+#include "Menu.hpp"
+
+Menu::Menu()
+{
+}
+
+Menu::~Menu()
+{
+}
