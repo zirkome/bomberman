25604339a557cd216fedf64ec732ff3322d250cf
Guillaume Fillon <guillaume@posteo.de>
2014-05-08 22:22:32 +0200

    Add ISerializable interface
    See include/ISerializable.hpp for the documentation

--- include/ISerializable.hpp
+++ include/ISerializable.hpp
@@ -1,13 +1,29 @@
 #ifndef _ISERIALIZABLE_H_
 # define _ISERIALIZABLE_H_
 
+# include <istream>
+
+/**
+ * ISerializable is a generic class interface
+ * which allow you serialize/deserialize your object
+ */
 class ISerializable
 {
 public:
   virtual ~ISerializable() {}
 
 public:
+  /**
+   * This method is use to serialize your object
+   * in an output stream
+   */
+  virtual void writeObject(std::ostream& os) = 0;
 
+  /**
+   * This method is use to deserialize your object
+   * from an input stream
+   */
+  virtual void readObject(std::istream& is) = 0;
 };
 
 #endif /* _ISERIALIZABLE_H_ */
