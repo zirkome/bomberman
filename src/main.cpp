d5bcce4bc764b360327a6d3fd801bab8274fd5e3
fabien.linardon <fabien.linardon@epitech.eu>
2014-05-14 09:22:38 +0200

    main.cpp updated

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,18 @@
 
 int main()
 {
-  // try
-  //   {
-  //     GameEngine intro;
+  try
+    {
+      GameEngine intro;
 
-  //     if (intro.initialize() == false)
-  //       throw std::runtime_error("Game initialization failed.");
-  //     while (intro.update() == true)
-  //       intro.draw();
-  //   }
-  // catch (std::exception& e)
-  //   {
-  //     std::cerr << e.what() << std::endl;
-  //     return (1);
-  //   }
-  Map	map(20, 20);
-
-  map.displayDebugMap();
-  return (0);
+      if (intro.initialize() == false)
+        throw std::runtime_error("Game initialization failed.");
+      while (intro.update() == true)
+        intro.draw();
+    }
+  catch (std::exception& e)
+    {
+      std::cerr << e.what() << std::endl;
+      return (1);
+    }
 }
