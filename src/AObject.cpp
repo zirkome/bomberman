5284bf47e160aca91a2906fc4b7d4bedbe35bd28
izissise <hugues.m@hotmail.fr>
2014-05-17 17:22:28 +0200

    Fix rotation, see http://gamedevlab.epitech.net/forum/index.php/595/information-rotate-un-objet-en-mouvement

--- src/AObject.cpp
+++ src/AObject.cpp
@@ -28,17 +28,14 @@ void AObject::scale(glm::vec3 const& scale)
 
 glm::mat4 AObject::getTransformation()
 {
-  glm::mat4 transform(1); // On cree une matrice identite
+  glm::mat4 transform(1);
 
-  // On applique ensuite les rotations selon les axes x, y et z
+  transform = glm::translate(transform, _position);
+
   transform = glm::rotate(transform, _rotation.x, glm::vec3(1, 0, 0));
   transform = glm::rotate(transform, _rotation.y, glm::vec3(0, 1, 0));
   transform = glm::rotate(transform, _rotation.z, glm::vec3(0, 0, 1));
 
-  // On effectue ensuite la translation
-  transform = glm::translate(transform, _position);
-
-  // Et pour finir, on fait la mise a l'echelle
   transform = glm::scale(transform, _scale);
   return (transform);
 }
