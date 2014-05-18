bb467f4833f97602f9f700a96b216d926bcf0e31
izissise <hugues.m@hotmail.fr>
2014-05-18 16:59:50 +0200

    Corrected triangle vertices for pan

--- src/Pan.cpp
+++ src/Pan.cpp
@@ -17,14 +17,21 @@ Pan::~Pan()
 
 bool Pan::initialize()
 {
-  _geometry.pushVertex(glm::vec3(-1.0f, -1.0f, 0.0f));
-  _geometry.pushVertex(glm::vec3(-1.0f, 1.0f, 0.0f));
   _geometry.pushVertex(glm::vec3(1.0f, 1.0f, 0.0f));
+  _geometry.pushVertex(glm::vec3(-1.0f, 1.0f, 0.0f));
+  _geometry.pushVertex(glm::vec3(-1.0f, -1.0f, 0.0f));
+
+  _geometry.pushVertex(glm::vec3(-1.0f, -1.0f, 0.0f));
   _geometry.pushVertex(glm::vec3(1.0f,  -1.0f, 0.0f));
+  _geometry.pushVertex(glm::vec3(1.0f, 1.0f, 0.0f));
+
   _geometry.pushUv(glm::vec2(0.0f, 0.0f));
   _geometry.pushUv(glm::vec2(1.0f, 0.0f));
   _geometry.pushUv(glm::vec2(1.0f, 1.0f));
+
+  _geometry.pushUv(glm::vec2(1.0f, 1.0f));
   _geometry.pushUv(glm::vec2(0.0f, 1.0f));
+  _geometry.pushUv(glm::vec2(0.0f, 0.0f));
 
   _geometry.build();
   return (true);
