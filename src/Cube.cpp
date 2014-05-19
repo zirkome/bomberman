6819e250ee32763f3608ed84748b1c101542a2ef
izissise <hugues.m@hotmail.fr>
2014-05-19 22:08:32 +0200

    fix cube normals

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -40,19 +40,19 @@ bool Cube::initialize()
     glm::vec3(0.000000, -1.000000, 0.000000)
   };
 
-  _geometry.pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(-normals[0]);
-  _geometry.pushVertex(vertice[0]).pushUv(uv[1]).pushNormal(-normals[0]);
-  _geometry.pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(-normals[0]);
-  _geometry.pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(-normals[0]);
-  _geometry.pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(-normals[0]);
-  _geometry.pushVertex(vertice[7]).pushUv(uv[3]).pushNormal(-normals[0]);
+  _geometry.pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
+  _geometry.pushVertex(vertice[0]).pushUv(uv[1]).pushNormal(normals[0]);
+  _geometry.pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
+  _geometry.pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
+  _geometry.pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
+  _geometry.pushVertex(vertice[7]).pushUv(uv[3]).pushNormal(normals[0]);
 
-  _geometry.pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(-normals[1]);
-  _geometry.pushVertex(vertice[6]).pushUv(uv[1]).pushNormal(-normals[1]);
-  _geometry.pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(-normals[1]);
-  _geometry.pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(-normals[1]);
-  _geometry.pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(-normals[1]);
-  _geometry.pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(-normals[1]);
+  _geometry.pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
+  _geometry.pushVertex(vertice[6]).pushUv(uv[1]).pushNormal(normals[1]);
+  _geometry.pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
+  _geometry.pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
+  _geometry.pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
+  _geometry.pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[1]);
 
   _geometry.pushVertex(vertice[1]).pushUv(uv[0]).pushNormal(normals[2]);
   _geometry.pushVertex(vertice[5]).pushUv(uv[1]).pushNormal(normals[2]);
@@ -75,12 +75,12 @@ bool Cube::initialize()
   _geometry.pushVertex(vertice[6]).pushUv(uv[2]).pushNormal(normals[6]);
   _geometry.pushVertex(vertice[5]).pushUv(uv[3]).pushNormal(normals[6]);
 
-  _geometry.pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(-normals[7]);
-  _geometry.pushVertex(vertice[1]).pushUv(uv[1]).pushNormal(-normals[7]);
-  _geometry.pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(-normals[7]);
-  _geometry.pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(-normals[7]);
-  _geometry.pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(-normals[7]);
-  _geometry.pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(-normals[7]);
+  _geometry.pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
+  _geometry.pushVertex(vertice[1]).pushUv(uv[1]).pushNormal(normals[7]);
+  _geometry.pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
+  _geometry.pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
+  _geometry.pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
+  _geometry.pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[7]);
 
   _geometry.build();
   return (true);
