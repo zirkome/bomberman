38a57c0e51c8cb7e608d51bbe3f7c989ee53fe12
debas_e <debas_e@epitech.eu>
2014-05-20 16:19:20 +0200

    change fontext class

--- src/FontText.cpp
+++ src/FontText.cpp
@@ -20,42 +20,28 @@ void FontText::displayText(const std::string &str, const glm::vec3 &pos, int siz
 
   for (unsigned int i = 0 ; i < str.length() ; i++)
     {
-      glm::vec3 vertex_up_left    = glm::vec3( pos.x+i*size     , pos.y+size , pos.z);
-      glm::vec3 vertex_up_right   = glm::vec3( pos.x+i*size+size, pos.y+size , pos.z);
-      glm::vec3 vertex_down_right = glm::vec3( pos.x+i*size+size, pos.y      , pos.z);
-      glm::vec3 vertex_down_left  = glm::vec3( pos.x+i*size     , pos.y      , pos.z);
+      float uv_x = (str[i] % 16) / 16.0f;
+      float uv_y = (str[i] / 16) / 16.0f;
 
-      geometry.pushVertex(vertex_up_left   );
-      geometry.pushVertex(vertex_down_left );
-      geometry.pushVertex(vertex_up_right  );
+      geometry.pushVertex(glm::vec3(pos.x + i * size, pos.y + size , pos.z));
+      geometry.pushVertex(glm::vec3(pos.x + i * size, pos.y, pos.z));
+      geometry.pushVertex(glm::vec3(pos.x + i * size + size, pos.y + size, pos.z));
+      geometry.pushVertex(glm::vec3(pos.x + i * size + size, pos.y, pos.z));
+      geometry.pushVertex(glm::vec3(pos.x + i * size + size, pos.y + size, pos.z));
+      geometry.pushVertex(glm::vec3(pos.x + i * size, pos.y, pos.z));
 
-      geometry.pushVertex(vertex_down_right);
-      geometry.pushVertex(vertex_up_right);
-      geometry.pushVertex(vertex_down_left);
-
-      char character = str[i];
-      float uv_x = (character % 16) / 16.0f;
-      float uv_y = (character / 16) / 16.0f;
-
-      glm::vec2 uv_up_left    = glm::vec2( uv_x           , 1 - uv_y );
-      glm::vec2 uv_up_right   = glm::vec2( uv_x+1.0f/16.0f, 1 - uv_y );
-      glm::vec2 uv_down_right = glm::vec2( uv_x+1.0f/16.0f, 1 - (uv_y + 1.0f/16.0f) );
-      glm::vec2 uv_down_left  = glm::vec2( uv_x           , 1 - (uv_y + 1.0f/16.0f) );
-
-      geometry.pushUv(uv_up_left   );
-      geometry.pushUv(uv_down_left );
-      geometry.pushUv(uv_up_right  );
-
-      geometry.pushUv(uv_down_right);
-      geometry.pushUv(uv_up_right);
-      geometry.pushUv(uv_down_left);
+      geometry.pushUv(glm::vec2(uv_x, 1.0 - uv_y));
+      geometry.pushUv(glm::vec2(uv_x, 1 - (uv_y + 1.0 / 16.0)));
+      geometry.pushUv(glm::vec2(uv_x + 1.0 / 16.0, 1 - uv_y));
+      geometry.pushUv(glm::vec2(uv_x + 1.0 / 16.0, 1 - (uv_y + 1.0 / 16.0)));
+      geometry.pushUv(glm::vec2(uv_x + 1.0 / 16.0, 1 - uv_y ));
+      geometry.pushUv(glm::vec2(uv_x, 1 - (uv_y + 1.0 / 16.0)));
     }
+
   geometry.build();
   _texture.bind();
 
-  // glDisable(GL_DEPTH)
   glEnable(GL_BLEND);
-
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
   geometry.draw(*shader, glm::mat4(1), GL_TRIANGLES);
