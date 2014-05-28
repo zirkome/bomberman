a98aafc5bf0b8e2858e704a6e36bdf231d569144
maxime94 <maxime.l@live.fr>
2014-05-28 15:16:29 +0200

    Opti map

--- src/Game.cpp
+++ src/Game.cpp
@@ -61,7 +61,7 @@ Game::Game(const glm::ivec2& win, int numberPlayer, int numberIA, std::vector<st
 void Game::init(glm::ivec2 win)
 {
   /* TODO : init game and load 3d models */
-  glm::vec2 playerPos = _players.front()->getPos();
+  //  glm::vec2 playerPos = _players.front()->getPos();
 
   //_cam = new FreeCam;
   // _cam = new BasicCam(glm::vec3(playerPos.x, playerPos.y, 0), 10, 3);
@@ -111,6 +111,8 @@ void Game::drawGame(UNUSED gdl::Input &input, gdl::Clock const &clock)
 {
   gdl::AShader *shader = _ogl.getShader();
   gdl::AShader *hudshader = _ogl.getHudShader();
+  glm::vec2 posObject(0, 0);
+  int rayon = 5;
 
   _ogl.startFrame();
   shader->setUniform("view", _cam->project());
@@ -121,8 +123,13 @@ void Game::drawGame(UNUSED gdl::Input &input, gdl::Clock const &clock)
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_REPEAT);
   _ground->draw(shader, clock);
 
+  glm::vec2 posPlayer = _players[0]->getPos();
   for (Map::iterator it = _currentMap->begin(); it != _currentMap->end(); ++it)
-    (*it)->draw(shader, clock);
+    {
+      posObject = (*it)->getPos();
+      if ((posObject.x < posPlayer.x + rayon && posObject.x > posPlayer.x - rayon && posObject.y < posPlayer.y + rayon && posObject.y > posPlayer.y - rayon))
+	(*it)->draw(shader, clock);
+    }
 
   glm::mat4 tmpMat =  glm::translate(glm::mat4(1), glm::vec3(0.0f, 1.0f, 0.0f));
   tmpMat = glm::scale(tmpMat, glm::vec3(16, 16, 16));
