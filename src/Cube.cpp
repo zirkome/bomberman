0aaac2a81a5dd69a4fa29f71f875e8ea83855fd4
izissise <hugues.m@hotmail.fr>
2014-05-30 02:58:54 +0200

    temporary fix to avoid create 1 million geometry !

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -1,13 +1,15 @@
 #include "Cube.hpp"
 
+static gdl::Geometry* geometry = NULL;
+
 Cube::Cube(gdl::Geometry* geometry)
-  : _geometry(geometry)
 {
+  _geometry = geometry;
 }
 
 Cube::Cube(const Cube& c)
-  : _geometry(c._geometry)
 {
+  _geometry = c._geometry;
 }
 
 const Cube& Cube::operator=(const Cube& c)
@@ -18,80 +20,86 @@ const Cube& Cube::operator=(const Cube& c)
 
 Cube::Cube()
 {
-  _geometry = new gdl::Geometry;
+  if (!geometry)
+    {
 
-  glm::vec3 vertice[] = {
-    glm::vec3(1.000000, -1.000000, -1.000000),
-    glm::vec3(1.000000, -1.000000, 1.000000),
-    glm::vec3(-1.000000, -1.000000, 1.000000),
-    glm::vec3(-1.000000, -1.000000, -1.000000),
-    glm::vec3(1.000000, 1.000000, -1.000000),
-    glm::vec3(1.000000, 1.000000, 1.000000),
-    glm::vec3(-1.000000, 1.000000, 1.000000),
-    glm::vec3(-1.000000, 1.000000, -1.000000)
-  };
+      geometry = new gdl::Geometry;
+      _geometry = geometry;
 
-  glm::vec2 uv[] = {
-    glm::vec2(0.000000, 0.000000),
-    glm::vec2(1.000000, 0.000000),
-    glm::vec2(1.000000, 1.000000),
-    glm::vec2(0.000000, 1.000000)
-  };
+      glm::vec3 vertice[] = {
+        glm::vec3(1.000000, -1.000000, -1.000000),
+        glm::vec3(1.000000, -1.000000, 1.000000),
+        glm::vec3(-1.000000, -1.000000, 1.000000),
+        glm::vec3(-1.000000, -1.000000, -1.000000),
+        glm::vec3(1.000000, 1.000000, -1.000000),
+        glm::vec3(1.000000, 1.000000, 1.000000),
+        glm::vec3(-1.000000, 1.000000, 1.000000),
+        glm::vec3(-1.000000, 1.000000, -1.000000)
+      };
 
-  glm::vec3 normals[] = {
-    glm::vec3(0.000000, 0.000000, -1.000000),
-    glm::vec3(-1.000000, 0.000000, 0.000000),
-    glm::vec3(0.000000, 0.000000, 1.000000),
-    glm::vec3(0.000000, 0.000000, 1.000000),
-    glm::vec3(1.000000, 0.000000, 0.000000),
-    glm::vec3(1.000000, 0.000000, 0.000000),
-    glm::vec3(0.000000, 1.000000, 0.000000),
-    glm::vec3(0.000000, -1.000000, 0.000000)
-  };
+      glm::vec2 uv[] = {
+        glm::vec2(0.000000, 0.000000),
+        glm::vec2(1.000000, 0.000000),
+        glm::vec2(1.000000, 1.000000),
+        glm::vec2(0.000000, 1.000000)
+      };
 
-  _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
-  _geometry->pushVertex(vertice[0]).pushUv(uv[1]).pushNormal(normals[0]);
-  _geometry->pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
-  _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
-  _geometry->pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
-  _geometry->pushVertex(vertice[7]).pushUv(uv[3]).pushNormal(normals[0]);
+      glm::vec3 normals[] = {
+        glm::vec3(0.000000, 0.000000, -1.000000),
+        glm::vec3(-1.000000, 0.000000, 0.000000),
+        glm::vec3(0.000000, 0.000000, 1.000000),
+        glm::vec3(0.000000, 0.000000, 1.000000),
+        glm::vec3(1.000000, 0.000000, 0.000000),
+        glm::vec3(1.000000, 0.000000, 0.000000),
+        glm::vec3(0.000000, 1.000000, 0.000000),
+        glm::vec3(0.000000, -1.000000, 0.000000)
+      };
 
-  _geometry->pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
-  _geometry->pushVertex(vertice[6]).pushUv(uv[1]).pushNormal(normals[1]);
-  _geometry->pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
-  _geometry->pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
-  _geometry->pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
-  _geometry->pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
+      _geometry->pushVertex(vertice[0]).pushUv(uv[1]).pushNormal(normals[0]);
+      _geometry->pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
+      _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[0]);
+      _geometry->pushVertex(vertice[3]).pushUv(uv[2]).pushNormal(normals[0]);
+      _geometry->pushVertex(vertice[7]).pushUv(uv[3]).pushNormal(normals[0]);
 
-  _geometry->pushVertex(vertice[1]).pushUv(uv[0]).pushNormal(normals[2]);
-  _geometry->pushVertex(vertice[5]).pushUv(uv[1]).pushNormal(normals[2]);
-  _geometry->pushVertex(vertice[2]).pushUv(uv[3]).pushNormal(normals[2]);
-  _geometry->pushVertex(vertice[5]).pushUv(uv[1]).pushNormal(normals[3]);
-  _geometry->pushVertex(vertice[6]).pushUv(uv[2]).pushNormal(normals[3]);
-  _geometry->pushVertex(vertice[2]).pushUv(uv[3]).pushNormal(normals[3]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[6]).pushUv(uv[1]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[0]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[7]).pushUv(uv[2]).pushNormal(normals[1]);
+      _geometry->pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[1]);
 
-  _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[4]);
-  _geometry->pushVertex(vertice[4]).pushUv(uv[1]).pushNormal(normals[4]);
-  _geometry->pushVertex(vertice[1]).pushUv(uv[3]).pushNormal(normals[4]);
-  _geometry->pushVertex(vertice[4]).pushUv(uv[1]).pushNormal(normals[5]);
-  _geometry->pushVertex(vertice[5]).pushUv(uv[2]).pushNormal(normals[5]);
-  _geometry->pushVertex(vertice[1]).pushUv(uv[3]).pushNormal(normals[5]);
+      _geometry->pushVertex(vertice[1]).pushUv(uv[0]).pushNormal(normals[2]);
+      _geometry->pushVertex(vertice[5]).pushUv(uv[1]).pushNormal(normals[2]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[3]).pushNormal(normals[2]);
+      _geometry->pushVertex(vertice[5]).pushUv(uv[1]).pushNormal(normals[3]);
+      _geometry->pushVertex(vertice[6]).pushUv(uv[2]).pushNormal(normals[3]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[3]).pushNormal(normals[3]);
 
-  _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[6]);
-  _geometry->pushVertex(vertice[7]).pushUv(uv[1]).pushNormal(normals[6]);
-  _geometry->pushVertex(vertice[5]).pushUv(uv[3]).pushNormal(normals[6]);
-  _geometry->pushVertex(vertice[7]).pushUv(uv[1]).pushNormal(normals[6]);
-  _geometry->pushVertex(vertice[6]).pushUv(uv[2]).pushNormal(normals[6]);
-  _geometry->pushVertex(vertice[5]).pushUv(uv[3]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[4]);
+      _geometry->pushVertex(vertice[4]).pushUv(uv[1]).pushNormal(normals[4]);
+      _geometry->pushVertex(vertice[1]).pushUv(uv[3]).pushNormal(normals[4]);
+      _geometry->pushVertex(vertice[4]).pushUv(uv[1]).pushNormal(normals[5]);
+      _geometry->pushVertex(vertice[5]).pushUv(uv[2]).pushNormal(normals[5]);
+      _geometry->pushVertex(vertice[1]).pushUv(uv[3]).pushNormal(normals[5]);
 
-  _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
-  _geometry->pushVertex(vertice[1]).pushUv(uv[1]).pushNormal(normals[7]);
-  _geometry->pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
-  _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
-  _geometry->pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
-  _geometry->pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[4]).pushUv(uv[0]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[7]).pushUv(uv[1]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[5]).pushUv(uv[3]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[7]).pushUv(uv[1]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[6]).pushUv(uv[2]).pushNormal(normals[6]);
+      _geometry->pushVertex(vertice[5]).pushUv(uv[3]).pushNormal(normals[6]);
 
-  _geometry->build();
+      _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[1]).pushUv(uv[1]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[0]).pushUv(uv[0]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[2]).pushUv(uv[2]).pushNormal(normals[7]);
+      _geometry->pushVertex(vertice[3]).pushUv(uv[3]).pushNormal(normals[7]);
+
+      _geometry->build();
+    }
+  _geometry = geometry;
 }
 
 Cube::~Cube()
