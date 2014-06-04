9075d5eedf88944ae4698ac647f1da1365b087df
maxime94 <maxime.l@live.fr>
2014-06-04 23:03:00 +0200

    add base of ia

--- script/medium.lua
+++ script/medium.lua
@@ -1,17 +1,57 @@
+newObjectif = function(x, y, thisptr)
+      z = math.random(1, 7)
+      if z == 1 then
+      	 iaAction(5, thisptr)
+      end
+      a = iaGetMap(x-1, y, thisptr)
+      b = iaGetMap(x+1, y, thisptr)
+      c = iaGetMap(x, y+1, thisptr)
+      d = iaGetMap(x, y-1, thisptr)
+      if a > 2 then
+          return x-1, y
+      end
+      if b > 2 then
+          return x+1, y
+      end
+      if c > 2 then
+          return x, y+1
+      end
+      if d > 2 then
+          return x, y-1
+      end
+      io.write('ret' .. a .. b .. c .. d .. '\n')
+      return x, y
+end
+
+goToObjectif = function(objectifX, objectifY, x, y)
+      io.write('[Lua] IA at <' .. x .. ', ' .. y .. '> obj <' .. objectifX .. ', ' .. objectifY .. '>\n')
+      if objectifX < x then
+      	 return 4
+      end
+      if objectifX > x then
+      	 return 3
+      end
+      if objectifY < y then
+      	 return 2
+      end
+      if objectifY > y then
+      	 return 1
+      end
+      return 0
+end
+
 iaLaunch(thisptr)
 io.write('[Lua] Hello,\n')
 x,y = iaGetPos(thisptr)
-z = iaGetMap(2, 2, thisptr)
-io.write('[Lua] IA create at <' .. x .. ', ' .. y .. '> : ' .. z .. '\n')
-a = x
-b = y
-mov = 1
+io.write('[Lua] IA create at <' .. x .. ', ' .. y .. '>\n')
+objectifX = math.floor(x)
+objectifY = math.floor(y)
+mov = 0
 while 1 do
       x,y = iaGetPos(thisptr)
-      if x == a and y == b then
-	   mov = math.random(1, 4)
+      if math.floor(x) == objectifX and math.floor(y) == objectifY then
+	   objectifX, objectifY = newObjectif(math.floor(x), math.floor(y), thisptr)
       end
+      mov = goToObjectif(objectifX, objectifY, x, y)
       iaAction(mov, thisptr)
-      a = x
-      b = y
 end
