a6a9a1aa68569a7a278ca281b21fd24c08e7d5f9
Guillaume Fillon <guillaume@posteo.de>
2014-06-12 19:04:52 +0200

    Fix Bomb range to be subject compliant

--- src/APlayer.cpp
+++ src/APlayer.cpp
@@ -7,7 +7,7 @@ APlayer::APlayer(const glm::vec2 &pos, Map *map, const glm::vec4& color, const s
   _max_bomb = 1;
 
   _stock_bomb = _max_bomb;
-  _bomb_range = 2;
+  _bomb_range = 3;
 
   _obj = new GameModel(RES_MODEL "marvin.fbx");
   (*_obj)->setCurrentSubAnim("walk");
